3207d0fff8d9e511f97e3aed2c090e106539ac3b
santiago817 <santiago_ivan_copaexample@hotmail.com>
2025-12-02 10:42:53 -0300

    Header + Footer y inicio + vidoe de youtube

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import LandingPage from "./components/LandingPage";
 
 export default function App() {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
+      <Header />
 
+      <LandingPage />
+
+      <Footer />
     </div>
-  )
+  );
 }
